3318b46c2d9ad8763d9293004c64464496068638
Rishabh Thakur <rishabhthakur046@gmail.com>
2024-02-29 01:40:52 +0530

    third commit

--- Abc.java
+++ Abc.java
@@ -2,8 +2,8 @@ public class Abc{
 	public static void main(String args[])
 	{
 		System.out.println("This is ruuning and compiling by jenkins");
-		System.out.print("This is to check if if the jenkins is working fine.");
-		
+		System.out.println("This is to check if if the jenkins is working fine.");
+		System.out.print("now the final test");
 
 	}
 }
