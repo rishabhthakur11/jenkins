d1d0dd6b17c1e9881fcbf741f06d92b175ae666a
Rishabh Thakur <rishabhthakur046@gmail.com>
2024-02-29 12:26:20 +0530

    evaluation commit

--- Abc.java
+++ Abc.java
@@ -3,7 +3,8 @@ public class Abc{
 	{
 		System.out.println("This is ruuning and compiling by jenkins");
 		System.out.println("This is to check if if the jenkins is working fine.");
-		System.out.print("now the final test");
+		System.out.println("now the final test");
+		System.out.println("Just to demonstrate the working");
 
 	}
 }
